5f000d07e18c69b97eb8c9cd554023ec10a40b88
Tim Evko <timevko@gmail.com>
2016-08-08 20:11:33 -0400

    initialize

---
+++ JankFreeScroll.js
@@ -0,0 +1,28 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        module.exports = factory();
+    } else {
+        root.jankfreescroll = factory();
+  }
+}((this || window), function () {
+
+    const jankfreescroll = config => {
+		const self = {};
+		const onScroll = ({onScrollDown, onScrollUp} = {}, lastScrollPos = window.pageYOffset) => {
+			  if (lastScrollPos < window.pageYOffset) {
+				  typeof onScrollDown !== 'undefined' && onScrollDown()
+			} else if (lastScrollPos > window.pageYOffset) {
+				  typeof onScrollUp !== 'undefined' && onScrollUp();
+			}
+			self.frameID = window.requestAnimationFrame(onScroll.bind(this, {onScrollDown, onScrollUp}, window.pageYOffset));
+		};
+		//invoke outer function, masking ugly bind stuff and kicking off recursive inner function call
+		window.requestAnimationFrame(onScroll.bind(this, config))
+		//return object containing rAF ID
+		return self
+	};
+	
+	return jankfreescroll
+}));
