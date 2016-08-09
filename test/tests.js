2acc74db3e1f5a1e9ea00bf5675edd18ae33fcba
Tim Evko <timevko@gmail.com>
2016-08-08 21:31:23 -0400

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -6,40 +6,47 @@
 */
 window.addEventListener('load', function() {
 
-	QUnit.module('JankFreeScroll', {
-	    beforeEach: function () {
-	      window.s = jankfreescroll({
-				onScrollDown() {
-					document.body.style.backgroundColor = 'white';
-				},
-				onScrollUp() {
-					document.body.style.backgroundColor = 'orange';
-				}
-			});
-	    },
-	    afterEach: function () {
-	      window.cancelAnimationFrame(window.s.frameID);
-	    }
-	});
-
 	QUnit.test( "changes bg on scrolldown", function( assert ) {
-	  
-	  window.scrollTo( 0, 1000 );
+	  window.s = jankfreescroll({
+		onScrollDown() {
+			document.body.style.backgroundColor = 'white';
+		},
+		onScrollUp() {
+			document.body.style.backgroundColor = 'orange';
+		}
+	 });
+	 window.scrollTo( 0, 1000 );
+	 window.cancelAnimationFrame(window.s.frameID);
 	  assert.equal( document.body.style.backgroundColor, 'white', 'bg should be white' );
 	  
 	});
 
 	QUnit.test( "change bg on scrollup", function( assert ) {
-	  
+	   window.s = jankfreescroll({
+		onScrollDown() {
+			document.body.style.backgroundColor = 'white';
+		},
+		onScrollUp() {
+			document.body.style.backgroundColor = 'orange';
+		}
+	 });
 	  window.scrollTo( 0, 1000 );
 	  window.scrollTo( 0, 0 );
+	  window.cancelAnimationFrame(window.s.frameID);
 
 	  assert.equal( document.body.style.backgroundColor, 'orange', 'bg should be orange' );
 	});
 
 	QUnit.test( "should be cancelable", function( assert ) {
 
-	  
+	   window.s = jankfreescroll({
+		onScrollDown() {
+			document.body.style.backgroundColor = 'white';
+		},
+		onScrollUp() {
+			document.body.style.backgroundColor = 'orange';
+		}
+	 });
 	  window.cancelAnimationFrame(window.s.frameID);
 
 	  window.scrollTo( 0, 1000 );
