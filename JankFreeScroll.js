7265226911a4a19d7018ee937e20d189d4d1729d
Tim Evko <timevko@gmail.com>
2016-08-08 20:17:33 -0400

    fix indents

--- JankFreeScroll.js
+++ JankFreeScroll.js
@@ -1,25 +1,25 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        root.jankfreescroll = factory();
-  }
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		module.exports = factory();
+	} else {
+		root.jankfreescroll = factory();
+	}
 }((this || window), function () {
 
-    const jankfreescroll = config => {
+	const jankfreescroll = config => {
 		const self = {};
 		const onScroll = ({onScrollDown, onScrollUp} = {}, lastScrollPos = window.pageYOffset) => {
-			  if (lastScrollPos < window.pageYOffset) {
-				  typeof onScrollDown !== 'undefined' && onScrollDown()
+			if (lastScrollPos < window.pageYOffset) {
+				typeof onScrollDown !== 'undefined' && onScrollDown()
 			} else if (lastScrollPos > window.pageYOffset) {
-				  typeof onScrollUp !== 'undefined' && onScrollUp();
+				typeof onScrollUp !== 'undefined' && onScrollUp();
 			}
 			self.frameID = window.requestAnimationFrame(onScroll.bind(this, {onScrollDown, onScrollUp}, window.pageYOffset));
 		};
 		//invoke outer function, masking ugly bind stuff and kicking off recursive inner function call
-		window.requestAnimationFrame(onScroll.bind(this, config))
+		window.requestAnimationFrame(onScroll.bind(this, config));
 		//return object containing rAF ID
 		return self
 	};
