ae992802afc22c643727dea16aa3cde320047854
Tim Evko <timevko@gmail.com>
2016-08-09 20:49:40 -0400

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -24,7 +24,11 @@ window.addEventListener('load', function() {
 
 	QUnit.test( "changes bg on scrolldown", function( assert ) {
 	  document.body.style.backgroundColor='black';
+	  window.scrollTo( 0, 0 );
 	  window.scrollTo( 0, 10000 );
+	  window.scrollTo( 0, 0 );
+	  window.scrollTo( 0, 10000 );
+	  
 	  assert.equal( document.body.style.backgroundColor, 'white', 'bg should be white' );
 	  
 	});
