db48b80907cfe70d87912a41aa75d53b62b613d9
Tim Evko <timevko@gmail.com>
2016-08-09 21:16:23 -0400

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -5,20 +5,19 @@
   Verify that JankFreeScroll fires on scroll up & down, and is cancelable
 */
 window.addEventListener('load', function() {
-	window.s = jankfreescroll({
-		onScrollDown() {
-			document.body.style.backgroundColor = 'white';
-		},
-		onScrollUp() {
-			document.body.style.backgroundColor = 'orange';
-		}
-	});
 	QUnit.module('jankfreescroll', {
 	    beforeEach: function () {
-	      
+	      window.s = jankfreescroll({
+			onScrollDown() {
+				document.body.style.backgroundColor = 'white';
+			},
+			onScrollUp() {
+				document.body.style.backgroundColor = 'orange';
+			}
+		});
 	    },
 	    afterEach: function () {
-	      
+	      window.cancelAnimationFrame(window.s.frameID);
 	    }
 	});
 
@@ -28,24 +27,14 @@ window.addEventListener('load', function() {
 	  window.scrollTo( 0, 100 );
 	  window.scrollTo( 0, 1000 );
 	  window.scrollTo( 0, 10000 );
-	  
-	  window.cancelAnimationFrame(window.s.frameID);
-	  window.scrollTo( 0, 0 );
 	  assert.equal( document.body.style.backgroundColor, 'white', 'bg should be white' );
 	  
 	});
 
 	QUnit.test( "change bg on scrollup", function( assert ) {
-		window.s = jankfreescroll({
-			onScrollDown() {
-				document.body.style.backgroundColor = 'white';
-			},
-			onScrollUp() {
-				document.body.style.backgroundColor = 'orange';
-			}
-		});
-	  
 	  window.scrollTo( 0, 1000 );
+	  window.scrollTo( 0, 100 )
+	  window.scrollTo( 0, 10 );
 	  window.scrollTo( 0, 0 );
 
 	  assert.equal( document.body.style.backgroundColor, 'orange', 'bg should be orange' );
